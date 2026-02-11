2f6256aae0eb3a75561786ef27c1aeede881f69c
Kristian Garder <garderkristian@gmail.com>
2026-02-11 13:52:53 +0100

    style(component-styling): :lipstick: update outline button styling

--- frontend/src/utils/router.ts
+++ frontend/src/utils/router.ts
@@ -2,15 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import ChangeNotesView from '../views/ChangeNotesView.vue'
 import ReleaseNotesView from '../views/ReleaseNotesView.vue'
+import ReleaseNoteView from '@/views/ReleaseNoteView.vue'
 
 export const routeNames = {
   releaseNotes: '/',
   changeNotes: '/change-notes',
+  releaseNote: '/release-note/:id',
+  //changeNote: '/change-note/:id',
 }
 
 const routes = [
   { path: routeNames.releaseNotes, component: ReleaseNotesView },
   { path: routeNames.changeNotes, component: ChangeNotesView },
+  { path: routeNames.releaseNote, component: ReleaseNoteView },
+  //{ path: routeNames.changeNote, component: ChangeNoteView },
 ]
 
 export const router = createRouter({
---
+++ frontend/src/views/ReleaseNoteView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+import { useRoute } from 'vue-router';
+
+import { ArrowLeft } from "lucide-vue-next"
+
+  const route = useRoute();
+  const id = route.params.id;
+  
+</script>
+
+<template>
+  <div class="flex">
+    <Button variant="outline" class="mb-4" @click="$router.push('/')"><ArrowLeft /> Previous</Button>
+    <h1>Release Note {{ id }}</h1>
+  </div>
+</template>
