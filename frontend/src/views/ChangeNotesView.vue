19f26fefb6e8e87cceb8fdb11caaafa3b22d094b
Stian <stianoyejenssen@gmail.com>
2026-02-12 14:05:53 +0100

    feat(change note): :sparkles: Add change note list page

--- frontend/src/views/ChangeNotesView.vue
+++ frontend/src/views/ChangeNotesView.vue
@@ -1,22 +1,74 @@
 <script setup lang="ts">
+import { useChangeNotes } from '@/api/change-note-api';
+import { createReleaseNoteMutation } from '@/api/release-note-api';
 import ChangeNoteCard from '@/components/ChangeNoteCard.vue';
+import Button from '@/components/ui/button/Button.vue';
+import Separator from '@/components/ui/separator/Separator.vue';
+import Spinner from '@/components/ui/spinner/Spinner.vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const { isLoading, isFetching, isError, data } = useChangeNotes();
+
+const selectedItems = ref<number[]>([]);
+
+const toggleSelection = (id: number) => {
+  if (selectedItems.value.includes(id)) {
+    selectedItems.value = selectedItems.value.filter(itemId => itemId !== id);
+  } else {
+    selectedItems.value.push(id);
+  }
+}
+
+const handlePublish = () => {
+  console.log('Publish button clicked. Selected change note IDs:', selectedItems.value);
+}
+
+const handleCreateChangeNote = () => {
+  console.log('Create Change Note button clicked');
+  
+}
+
+const createReleaseNoteMutationInstance = createReleaseNoteMutation((data: number) => {
+  router.push(`/release-notes/${data}`);
+})
+
+const handleCreateReleaseNote = () => {
+  console.log('Creating a Release Note with selected change notes. Selected change note IDs:', selectedItems.value);
+  createReleaseNoteMutationInstance.mutate()
+}
 
 </script>
 
 <template>
-  <div class="flex w-full justify-center">
-    <div class="flex flex-col gap-4 max-w-3xl">
-      <h1>Change Notes</h1>
-      <ChangeNoteCard
-        v-for="index in 10"
-        :key="index"
-        title="Test"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum  feugiat ornare leo non pharetra. Cras rutrum ultricies erat, eget  commodo quam porta ut. Quisque placerat metus vel consequat tempor.  Aliquam ac finibus mi, ac luctus lacus. Phasellus laoreet molestie  lectus. Nullam vel arcu eu arcu euismod commodo. Phasellus cursus,  tortor id interdum commodo, purus tortor placerat eros, a lacinia tortor sem a dui. Vestibulum eu congue orci, non lacinia nisi. Quisque viverra viverra ipsum ac tempor. Mauris finibus tincidunt dapibus."
-        :published="true"
-        company="HIG"
-        scope="Minor"
-        feature="Pick Planning"
-      />
+  <div class="min-h-full flex justify-center">
+    <div v-if="isLoading || isFetching">
+      <Spinner />
+    </div>
+
+    <p v-else-if="isError">Failed to load change notes. Please try again later.</p>
+
+    <div v-else class="flex gap-8 w-full flex-wrap justify-center mt-8">
+      <div class="p-4">
+        <h1 class="text-2xl">Change Notes</h1>
+      </div>
+
+      <div class="flex flex-col w-full gap-4 max-w-3xl">
+        <div class="w-full flex justify-end gap-4">
+          <Button variant="outline" @click="handlePublish">Publish</Button>
+          <Button variant="outline" @click="handleCreateChangeNote">Create Change Note</Button>
+          <Button variant="outline" @click="handleCreateReleaseNote">Create Release Note</Button>
+        </div>
+
+
+        <div v-for="changeNote in data" class="flex flex-col gap-4">
+          <ChangeNoteCard :key="changeNote.id" :selected="selectedItems.includes(changeNote.id)"
+            :changeNote="changeNote" @update:selected="toggleSelection(changeNote.id)" />
+          <Separator />
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
