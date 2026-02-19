8b65025803a8cbdcb96a8f22b3186ac5760cc181
Kristian Garder <garderkristian@gmail.com>
2026-02-19 11:08:10 +0100

    feat(component): :sparkles: Bugfix in multiselect to update v-model with changenotes

--- frontend/src/schemas.ts
+++ frontend/src/schemas.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod'
 
 export const EditReleaseNoteSchema = z.object({
-  id: z.number(),
   tag: z.string().min(1),
   summary: z.string().optional(),
   changeNotes: z.array(z.number()).optional(),
+  published: z.boolean(),
 })
 
 export const EditChangeNoteSchema = z.object({
