448aaa3a0f642819424d0e93cde800beabb39790
Ludvik Lund-Hole <ludvik.lund.hole@gmail.com>
2026-02-23 10:47:14 +0100

    feat(ci): :construction_worker: add action for eslint and build frontend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,27 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+export default [
+  { ignores: ['*.d.ts', '**/coverage', '**/dist'] },
+  eslint.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  ...eslintPluginVue.configs['flat/strongly-recommended-error'],
+  {
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        parser: typescriptEslint.parser,
+      },
+    },
+    rules: {
+      'vue/no-unused-vars': 'error'
+    },
+  },
+  eslintConfigPrettier
+];
