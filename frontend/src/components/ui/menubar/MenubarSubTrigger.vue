bced49367c154a0dd56e7b0c12243cfcb2e704c1
Kristian Garder <garderkristian@gmail.com>
2026-03-03 10:52:17 +0100

    feat(ux): :children_crossing: add language choice to header menu

---
+++ frontend/src/components/ui/menubar/MenubarSubTrigger.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { MenubarSubTriggerProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { reactiveOmit } from "@vueuse/core"
+import { ChevronLeft, ChevronRight } from "lucide-vue-next"
+import { MenubarSubTrigger, useForwardProps } from "reka-ui"
+import { cn } from "@/lib/utils"
+
+const props = defineProps<MenubarSubTriggerProps & { class?: HTMLAttributes["class"], inset?: boolean }>()
+
+const delegatedProps = reactiveOmit(props, "class", "inset")
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <MenubarSubTrigger
+    data-slot="menubar-sub-trigger"
+    :data-inset="inset ? '' : undefined"
+    v-bind="forwardedProps"
+    :class="cn(
+      'focus:text-text-primary data-[state=open]:bg-text-primary/10 mx-1 data-[state=open]:text-accent-foreground flex justify-end cursor-default items-center rounded-sm gap-2 px-2 py-1.5 text-sm outline-none select-none data-[inset]:pl-8',
+      props.class,
+    )"
+  >
+    <slot />
+    <ChevronLeft class="size-4" />
+  </MenubarSubTrigger>
+</template>
