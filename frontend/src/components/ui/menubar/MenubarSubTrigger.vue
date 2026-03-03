4e2d86f6c5c66a79b01138353aa76f91c78f5ccb
Kristian Garder <garderkristian@gmail.com>
2026-03-03 10:54:42 +0100

    refactor(refactor): :recycle: optimized imports

--- frontend/src/components/ui/menubar/MenubarSubTrigger.vue
+++ frontend/src/components/ui/menubar/MenubarSubTrigger.vue
@@ -2,7 +2,7 @@
 import type { MenubarSubTriggerProps } from "reka-ui"
 import type { HTMLAttributes } from "vue"
 import { reactiveOmit } from "@vueuse/core"
-import { ChevronLeft, ChevronRight } from "lucide-vue-next"
+import { ChevronLeft } from "lucide-vue-next"
 import { MenubarSubTrigger, useForwardProps } from "reka-ui"
 import { cn } from "@/lib/utils"
 
