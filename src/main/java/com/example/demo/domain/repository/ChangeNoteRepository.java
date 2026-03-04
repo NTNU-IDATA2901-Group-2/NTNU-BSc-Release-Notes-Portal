31ff0e12b5fa7557917938a670236e8d52cb11c7
Ludvik Lund-Hole <ludvik.lund.hole@gmail.com>
2026-03-04 14:12:14 +0100

    refactor(api): :recycle: streamline API usage in frontend

--- src/main/java/com/example/demo/domain/repository/ChangeNoteRepository.java
+++ src/main/java/com/example/demo/domain/repository/ChangeNoteRepository.java
@@ -32,12 +32,10 @@ public interface ChangeNoteRepository extends JpaRepository<ChangeNote, Long> {
       LEFT JOIN c.releaseNote r ON (r.archived = false)
       WHERE c.archived = false AND
         (:published IS NULL OR c.published = :published) AND
-        (:hasReleaseNote IS NULL OR (
-          r.archived = FALSE AND (
-            (:hasReleaseNote = TRUE AND c.releaseNote IS NOT NULL) OR
-            (:hasReleaseNote = FALSE AND c.releaseNote IS NULL)
-          )
-        )) AND
+        (:hasReleaseNote IS NULL OR
+          (:hasReleaseNote = TRUE AND c.releaseNote IS NOT NULL) OR
+          (:hasReleaseNote = FALSE AND c.releaseNote IS NULL)
+        ) AND
         (:customerIds IS NULL OR c.customer.id IN :customerIds) AND
         (:featureIds IS NULL OR c.feature.id IN :featureIds) AND
         (:scopeIds IS NULL OR c.scope.id IN :scopeIds) AND
