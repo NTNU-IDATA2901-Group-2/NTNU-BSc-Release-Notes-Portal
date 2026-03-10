8bb9ac83b09a558e648cc2733766378ba9732436
Stian <stianoyejenssen@gmail.com>
2026-03-10 10:42:55 +0100

    feat(change note): :sparkles: Allow for filtering by id when fetching change ntoes

---
+++ src/main/java/no/reliablesolutions/release_notes_portal/dto/ChangeNoteFilterOptionsDTO.java
@@ -0,0 +1,14 @@
+package no.reliablesolutions.release_notes_portal.dto;
+
+import java.util.List;
+
+public record ChangeNoteFilterOptionsDTO(
+      String query,
+      Boolean published,
+      Boolean hasReleaseNote,
+      List<Long> filteredIds,
+      List<Long> customerIds,
+      List<Long> featureIds,
+      List<Long> scopeIds,
+      List<Long> productIds
+) {}
