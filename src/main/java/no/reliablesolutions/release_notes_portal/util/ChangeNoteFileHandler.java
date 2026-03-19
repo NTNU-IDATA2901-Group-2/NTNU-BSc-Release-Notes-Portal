6c7194e2df892433df8617632527be8a0f4bd4f4
Ludvik Lund-Hole <ludvik.lund.hole@gmail.com>
2026-03-19 11:16:19 +0100

    chore(integration): :loud_sound: log error when encountering IO exceptions for change note file handler

--- src/main/java/no/reliablesolutions/release_notes_portal/util/ChangeNoteFileHandler.java
+++ src/main/java/no/reliablesolutions/release_notes_portal/util/ChangeNoteFileHandler.java
@@ -150,9 +150,9 @@ public class ChangeNoteFileHandler {
       }
       
     } catch (FileNotFoundException e) {
-      logger.error("Could not locate file {}", changeNoteFile.getName());
+      logger.error("Could not locate file {}", changeNoteFile.getName(), e);
     } catch (IOException e) {
-      logger.error("Error reading file {}", changeNoteFile.getName());
+      logger.error("Error reading file {}", changeNoteFile.getName(), e);
     } catch (ClassCastException e) {
       throw new InvalidChangeNoteYamlException("Invalid data type for one or more fields in the YAML file: " + e.getMessage());
     }
