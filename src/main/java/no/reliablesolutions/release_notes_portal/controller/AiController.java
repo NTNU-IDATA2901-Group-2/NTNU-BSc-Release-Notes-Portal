e2d7d43aef610df1fe0d5c0aacfdd91b48699be5
Kristian Garder <garderkristian@gmail.com>
2026-03-26 15:44:55 +0100

    clean and refactor

--- src/main/java/no/reliablesolutions/release_notes_portal/controller/AiController.java
+++ src/main/java/no/reliablesolutions/release_notes_portal/controller/AiController.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -23,7 +24,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PatchMapping;
 
  
 
@@ -88,7 +88,7 @@ public class AiController {
      * @return a ResponseEntity indicating the success of the update operation
      */
     @PreAuthorize("hasRole('ADMIN')")
-    @PatchMapping("/prompts")
+    @PutMapping("/prompts")
     public ResponseEntity<Void> updatePrompts(@RequestBody List<PromptDTO> prompts) {
         logger.info("Updating prompts");
         aiService.updatePrompts(prompts);
