cce9de6e547a81362b09dfd6b813f496c51f3ab7
Stian <stianoyejenssen@gmail.com>
2026-04-22 12:16:44 +0200

    feat(release note): :sparkles: Add feedback on release note edit page when selecting repository

--- frontend/src/components/releaseNote/ReleaseNoteEdit.vue
+++ frontend/src/components/releaseNote/ReleaseNoteEdit.vue
@@ -22,6 +22,7 @@ import Tooltip from '../ui/tooltip/Tooltip.vue';
 import TooltipTrigger from '../ui/tooltip/TooltipTrigger.vue';
 import TooltipContent from '../ui/tooltip/TooltipContent.vue';
 import DialogPrompt from '../DialogPrompt.vue';
+import { useGetGitRepositories } from '@/api/git-repository-api';
 
 const { t } = useI18n();
 
@@ -36,6 +37,7 @@ const releaseNote = props.releaseNote;
 
 const changeNoteIdsWithinReleaseNote = ref<number[]>(releaseNote.changeNotes?.map(cn => cn.id) || [])
 
+const { data: availableGitRepositories } = useGetGitRepositories()
 const { data: availableChangeNotes } = useGetChangeNotes()
 
 const gitRepository = ref<GitRepository | null>(null)
@@ -46,40 +48,40 @@ const { data: currentGitRepositoryChangeNotes } = useGetChangeNotes(params)
 
 const fromChangeNote = ref<ChangeNote | null>(null);
 const toChangeNote = ref<ChangeNote | null>(null);
-    
+
 const onChangeNotesUpdate = (value: number[]) => {
   changeNoteIdsWithinReleaseNote.value = value;
   fromChangeNote.value = null;
   toChangeNote.value = null;
 }
-    
+
 const onChangeNoteRangeChange = (gitRepositoryId: number) => {
   const fromChangeNoteValue = fromChangeNote.value
   const toChangeNoteValue = toChangeNote.value
   const fromIndex = availableChangeNotes.value?.findIndex((cn) => cn.id === fromChangeNoteValue?.id) ?? -1;
   const toIndex = availableChangeNotes.value?.findIndex((cn) => cn.id === toChangeNoteValue?.id) ?? -1;
-  
+
   if (gitRepositoryId !== -1) { // remove old change notes on the same repository before adding new range
     changeNoteIdsWithinReleaseNote.value = changeNoteIdsWithinReleaseNote.value.filter(id => {
       const cn = availableChangeNotes.value?.find(cn => cn.id === id)
       return cn?.gitRepositoryId !== gitRepositoryId
     })
   }
-  
+
   let newChangeNoteIds: number[];
   if (fromChangeNoteValue !== null && toChangeNoteValue !== null && fromIndex !== -1 && toIndex !== -1) {
-    newChangeNoteIds = availableChangeNotes.value?.map(cn => cn.id).slice(fromIndex, toIndex + 1) ?? []  
+    newChangeNoteIds = availableChangeNotes.value?.map(cn => cn.id).slice(fromIndex, toIndex + 1) ?? []
   } else if (fromChangeNoteValue !== null && toChangeNoteValue === null && fromIndex !== -1) {
-    newChangeNoteIds = availableChangeNotes.value?.map(cn => cn.id).slice(fromIndex) ?? []  
+    newChangeNoteIds = availableChangeNotes.value?.map(cn => cn.id).slice(fromIndex) ?? []
   } else if (fromChangeNoteValue === null && toChangeNoteValue !== null && toIndex !== -1) {
     newChangeNoteIds = availableChangeNotes.value?.map(cn => cn.id).slice(0, toIndex + 1) ?? []
   } else {
     newChangeNoteIds = []
   }
-  
+
   changeNoteIdsWithinReleaseNote.value = [...new Set([...(changeNoteIdsWithinReleaseNote.value ?? []), ...newChangeNoteIds])];
 }
-    
+
 const summarizeReleaseNote = useSummarizeChangeNotes({
   onSuccess: (summary) => {
     if (summary === undefined) {
@@ -95,9 +97,9 @@ const summarizeReleaseNote = useSummarizeChangeNotes({
 })
 
 const hasCommits = useGetHasCommits(changeNoteIdsWithinReleaseNote);
-const disableSummarizeButton = computed(() => hasCommits.isPending.value || hasCommits.isError.value || hasCommits.data.value !== true);  
+const disableSummarizeButton = computed(() => hasCommits.isPending.value || hasCommits.isError.value || hasCommits.data.value !== true);
 
-    
+
 const form = useForm({
   validationSchema: toTypedSchema(EditReleaseNoteSchema),
   initialValues: {
@@ -107,7 +109,7 @@ const form = useForm({
     published: releaseNote.published,
   }
 })
-    
+
 const onCancel = () => {
   cancelDialogOpen.value = true;
 }
@@ -138,139 +140,145 @@ const updateReleaseNoteMutation = useUpdateReleaseNote({
 
 const [tag] = form.defineField('tag');
 const [summary] = form.defineField('summary');
-  
+
 // Warn user of unsaved changes when trying to leave the page
 const beforeUnloadListener = (event: BeforeUnloadEvent) => {
-     event.preventDefault();
-    ;(event as unknown as { returnValue: string }).returnValue = ''
+  event.preventDefault();
+  ; (event as unknown as { returnValue: string }).returnValue = ''
 }
 
 onBeforeRouteLeave(() => {
-    return globalThis.confirm(t('changeNoteEdit.cancelDescription')) === true;
+  return globalThis.confirm(t('changeNoteEdit.cancelDescription')) === true;
 })
 
 onMounted(() => {
-    globalThis.addEventListener('beforeunload', beforeUnloadListener);
+  globalThis.addEventListener('beforeunload', beforeUnloadListener);
 })
 
 onBeforeUnmount(() => {
-    globalThis.removeEventListener('beforeunload', beforeUnloadListener);
+  globalThis.removeEventListener('beforeunload', beforeUnloadListener);
 })
 </script>
-    
-    
-    <template>
-      <DialogPrompt :open="cancelDialogOpen" :mode="'confirm'" :title-key="'releaseNoteEdit.cancelTitle'" :description-key="'releaseNoteEdit.cancelDescription'" @update:open="cancelDialogOpen = false" @confirm="cancelEdit" />
-      <div class="flex flex-col w-full items-center px-4 mb-20">
-        <div class="mb-4 absolute left-4 mt-4 lg:left-10 lg:mt-10 flex items-center gap-4">
-          <Button variant="outline" class="" @click="$router.back()">
-            <ArrowLeft />{{ t('button.previous') }}
-          </Button>
-          <Breadcrumb class="text-text-primary">
-            <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbLink href="/">{{ t('title.releaseNotes') }}</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                {{ releaseNote?.tag }}
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-        
-        <form class="w-full flex flex-col items-center" @submit="onSubmit">
-          <div 
-          ref="releaseNoteRef"
-          class="flex flex-col gap-16 flex-1 w-full items-center mt-16 mx-4 lg:w-4xl md:mt-42">
-          <div class="flex flex-col gap-4 w-full">
-            <div data-pdf-exclude class="flex sm:hidden gap-4 ml-auto">
-              <Button
-              :disabled="disableSummarizeButton"
-              type="button"
-              @click="summarizeReleaseNote.mutate(changeNoteIdsWithinReleaseNote)"
-              variant="glow"
-              >
-              {{t('button.summarize')}}
+
+
+<template>
+  <DialogPrompt 
+  :open="cancelDialogOpen" :mode="'confirm'"
+    :title-key="'releaseNoteEdit.cancelTitle'"
+    :description-key="'releaseNoteEdit.cancelDescription'" @update:open="cancelDialogOpen = false"
+    @confirm="cancelEdit" />
+  <div class="flex flex-col w-full items-center px-4 mb-20">
+    <div class="mb-4 absolute left-4 mt-4 lg:left-10 lg:mt-10 flex items-center gap-4">
+      <Button variant="outline" class="" @click="$router.back()">
+        <ArrowLeft />{{ t('button.previous') }}
+      </Button>
+      <Breadcrumb class="text-text-primary">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">{{ t('title.releaseNotes') }}</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            {{ releaseNote?.tag }}
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+
+    <form class="w-full flex flex-col items-center" @submit="onSubmit">
+      <div 
+      ref="releaseNoteRef"
+        class="flex flex-col gap-16 flex-1 w-full items-center mt-16 mx-4 lg:w-4xl md:mt-42">
+        <div class="flex flex-col gap-4 w-full">
+          <div data-pdf-exclude class="flex sm:hidden gap-4 ml-auto">
+            <Button 
+            :disabled="disableSummarizeButton" type="button"
+              @click="summarizeReleaseNote.mutate(changeNoteIdsWithinReleaseNote)" variant="glow">
+              {{ t('button.summarize') }}
               <Sparkles />
             </Button>
-            <Button
-            type="button"
-            class=""
-            variant="outline"
-            @click="onCancel">{{ t('button.cancel') }}
-            <Ban />
-          </Button>
-          <Button variant="outline" type="submit">{{
-            t('button.save') }}
-            <Save />
-          </Button>
-        </div>
-        <div class="flex flex-row items-center justify-between w-full">
-          <div class="flex items-center gap-4">
-            <div class="flex flex-col gap-1">
-              <h1 class="text-lg">{{ t('title.title') }}</h1>
-              <Input class="w-full" v-model="tag" :placeholder="t('placeholder.title')" />
+            <Button type="button" class="" variant="outline" @click="onCancel">{{ t('button.cancel')
+              }}
+              <Ban />
+            </Button>
+            <Button variant="outline" type="submit">{{
+              t('button.save') }}
+              <Save />
+            </Button>
+          </div>
+          <div class="flex flex-row items-center justify-between w-full">
+            <div class="flex items-center gap-4">
+              <div class="flex flex-col gap-1">
+                <h1 class="text-lg">{{ t('title.title') }}</h1>
+                <Input class="w-full" v-model="tag" :placeholder="t('placeholder.title')" />
+              </div>
+            </div>
+            <div data-pdf-exclude class="hidden sm:flex gap-4 mt-auto">
+              <Tooltip>
+                <TooltipTrigger as-child>
+                  <div class="inline-flex">
+                    <Button 
+                    :disabled="disableSummarizeButton" type="button"
+                      @click="summarizeReleaseNote.mutate(changeNoteIdsWithinReleaseNote)"
+                      variant="glow">
+                      {{ t('button.summarize') }}
+                      <Sparkles />
+                    </Button>
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent v-if="disableSummarizeButton">
+                  {{ t('tooltip.noCommits') }}
+                </TooltipContent>
+              </Tooltip>
+              <Button type="button" variant="outline" @click="onCancel">{{ t('button.cancel') }}
+                <Ban />
+              </Button>
+              <Button variant="outline" type="submit">{{
+                t('button.save') }}
+                <Save />
+              </Button>
             </div>
           </div>
-          <div data-pdf-exclude class="hidden sm:flex gap-4 mt-auto">
-            <Tooltip>
-              <TooltipTrigger as-child>
-                <div class = "inline-flex">
-                  <Button
-                    :disabled="disableSummarizeButton"
-                    type="button"
-                    @click="summarizeReleaseNote.mutate(changeNoteIdsWithinReleaseNote)"
-                    variant="glow"
-                  >
-                    {{t('button.summarize')}}
-                    <Sparkles />
-                  </Button>
-                </div>
-              </TooltipTrigger>
-              <TooltipContent v-if="disableSummarizeButton">
-                  {{ t('tooltip.noCommits') }}
-              </TooltipContent>
-            </Tooltip>
-          <Button 
-          type="button"
-          variant="outline"
-          @click="onCancel">{{ t('button.cancel') }}
-          <Ban />
-        </Button>
-        <Button variant="outline" type="submit">{{
-          t('button.save') }}
-          <Save />
-        </Button>
-      </div>
-    </div>
-    
-    <div class="flex flex-col gap-1">
-      <h1 class="text-lg">{{ t('title.description') }}</h1>
-      <Textarea 
-      class="w-full" v-model="summary"
-      :placeholder="t('placeholder.description')" />
-    </div>
-  </div>
-  <div class="flex flex-col w-full gap-10">
-    <div class="flex flex-col gap-4">
-      <div class="flex flex-col gap-1">
-        <h1 class="text-lg">{{ t('title.changeNotes') }}</h1>
-        <MultiselectChangeNotes @update:model-value="onChangeNotesUpdate" v-model="changeNoteIdsWithinReleaseNote" />
-      </div>
-      <div class="flex flex-col gap-1">
-        <h2 class="text-xl">{{ t('title.addFromRepository') }}</h2>
-        <div class="flex flex-col sm:flex-row gap-2 justify-between">
-          <SelectGitRepository v-model="gitRepository" />
-          <SelectChangeNotes :placeholder="t('title.from')" :disabled = "gitRepository === null" :change-notes="currentGitRepositoryChangeNotes ?? []" v-model="fromChangeNote" />
-          <SelectChangeNotes :placeholder="t('title.to')" :disabled = "gitRepository === null" :change-notes="currentGitRepositoryChangeNotes ?? []" v-model="toChangeNote" />
-          <Button class="w-45" :disabled = "gitRepository === null" type="button" variant="outline" @click.stop="onChangeNoteRangeChange(gitRepository?.id ?? -1)">{{ t('button.updateRange') }}</Button>
+
+          <div class="flex flex-col gap-1">
+            <h1 class="text-lg">{{ t('title.description') }}</h1>
+            <Textarea 
+            class="w-full" v-model="summary"
+              :placeholder="t('placeholder.description')" />
+          </div>
+        </div>
+        <div class="flex flex-col w-full gap-10">
+          <div class="flex flex-col gap-4">
+            <div class="flex flex-col gap-1">
+              <h1 class="text-lg">{{ t('title.changeNotes') }}</h1>
+              <MultiselectChangeNotes 
+              @update:model-value="onChangeNotesUpdate"
+                v-model="changeNoteIdsWithinReleaseNote" />
+            </div>
+            <div class="flex flex-col gap-1">
+              <h2 class="text-xl">{{ t('title.addFromRepository') }}</h2>
+              <p
+                v-if="availableGitRepositories == undefined || availableGitRepositories.length < 1">
+                {{ t('repositories.noGitRepositories') }}</p>
+              <div v-else class="flex flex-col sm:flex-row gap-2 justify-between">
+                <SelectGitRepository v-model="gitRepository" />
+                <SelectChangeNotes 
+                :placeholder="t('title.from')" :disabled="gitRepository === null"
+                  :change-notes="currentGitRepositoryChangeNotes ?? []" v-model="fromChangeNote" />
+                <SelectChangeNotes 
+                :placeholder="t('title.to')" :disabled="gitRepository === null"
+                  :change-notes="currentGitRepositoryChangeNotes ?? []" v-model="toChangeNote" />
+                <Button 
+                class="w-45" :disabled="gitRepository === null || currentGitRepositoryChangeNotes === undefined || currentGitRepositoryChangeNotes.length === 0" type="button"
+                  variant="outline"
+                  @click.stop="onChangeNoteRangeChange(gitRepository?.id ?? -1)">{{
+                    t('button.updateRange') }}</Button>
+              </div>
+              <p v-if="gitRepository !== null && (currentGitRepositoryChangeNotes === undefined || currentGitRepositoryChangeNotes.length === 0)">{{ t('repositories.noChangeNotesInRepository') }}</p>
+            </div>
+          </div>
         </div>
       </div>
-      
-    </div>
+    </form>
   </div>
-</div>
-</form>
-</div>
 </template>
