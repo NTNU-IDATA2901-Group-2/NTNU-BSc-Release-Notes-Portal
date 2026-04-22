4eb6e366150a63f71145c562bbca5bcc96502f05
Stian <stianoyejenssen@gmail.com>
2026-04-22 10:16:17 +0200

    feat(Change Note): :sparkles: Add descriptive tool tips to badges

--- frontend/src/components/ChangeNoteCard.vue
+++ frontend/src/components/ChangeNoteCard.vue
@@ -5,6 +5,7 @@ import type { ChangeNote } from '@/utils/types';
 import { Checkbox } from './ui/checkbox';
 import { useI18n } from 'vue-i18n';
 import { isAdmin } from '@/utils/keycloak';
+import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
 const props = defineProps<PrimitiveProps & {
   changeNote: ChangeNote
@@ -24,7 +25,7 @@ const handleCheckboxClick = (event: Event) => {
 
 <template>
   <RouterLink :to="`/change-notes/${changeNote.id}`" class="block">
-    
+
     <div
       class="flex flex-col p-4 gap-2 h-30 overflow-hidden text-wrap rounded-lg hover:bg-text-primary/10 transition-colors">
       <div class="flex justify-between">
@@ -36,20 +37,52 @@ const handleCheckboxClick = (event: Event) => {
 
           <h3 class="text-xl">{{ changeNote.reference }}</h3>
 
-          <Badge v-if="changeNote.customer" :variant="'outline'">{{ changeNote.customer.name }}</Badge>
+          <Tooltip v-if="changeNote.customer">
+            <TooltipTrigger as-child>
+              <Badge v-if="changeNote.customer" :variant="'outline'">{{ changeNote.customer.name }}
+              </Badge>
+            </TooltipTrigger>
+            <TooltipContent>
+              {{ t('title.customer') }}
+            </TooltipContent>
+          </Tooltip>
 
         </div>
         <div>
-          <Badge v-if="isAdmin" :variant="changeNote.published ? 'success' : 'destructive'">{{ changeNote.published
-            ? t('card.published') : t('card.private') }}</Badge>
+          <Badge v-if="isAdmin" :variant="changeNote.published ? 'success' : 'destructive'">{{
+            changeNote.published
+              ? t('card.published') : t('card.private') }}</Badge>
         </div>
       </div>
 
 
       <div class="flex flex-row gap-2 mt-auto">
-        <Badge v-if="changeNote.scope" :variant="'default'">{{ changeNote.scope.name }}</Badge>
-        <Badge v-if="changeNote.feature" :variant="'default'">{{ changeNote.feature.name }}</Badge>
-        <Badge v-if="changeNote.product" :variant="'default'">{{ changeNote.product.name }}</Badge>
+        <Tooltip v-if="changeNote.product">
+          <TooltipTrigger as-child>
+            <Badge class="h-6">{{ changeNote.product.name }}</Badge>
+          </TooltipTrigger>
+          <TooltipContent>
+            {{ t('title.product') }}
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip v-if="changeNote.scope">
+          <TooltipTrigger as-child>
+            <Badge class="h-6">{{ changeNote.scope.name }}</Badge>
+          </TooltipTrigger>
+          <TooltipContent>
+            {{ t('title.scope') }}
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip v-if="changeNote.feature">
+          <TooltipTrigger as-child>
+            <Badge class="h-6">{{ changeNote.feature.name }}</Badge>
+          </TooltipTrigger>
+          <TooltipContent>
+            {{ t('title.feature') }}
+          </TooltipContent>
+        </Tooltip>
       </div>
 
     </div>
