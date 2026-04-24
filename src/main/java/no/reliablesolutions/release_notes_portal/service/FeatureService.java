10f15941dd5a87e4a29c43c26184240051e0aca1
Stian <stianoyejenssen@gmail.com>
2026-04-24 10:20:34 +0200

    docs: :memo: Tweak documentation in FeatureService

--- src/main/java/no/reliablesolutions/release_notes_portal/service/FeatureService.java
+++ src/main/java/no/reliablesolutions/release_notes_portal/service/FeatureService.java
@@ -12,6 +12,9 @@ import no.reliablesolutions.release_notes_portal.dto.FeatureDTO;
 import no.reliablesolutions.release_notes_portal.exception.FailedToSaveEntityException;
 import no.reliablesolutions.release_notes_portal.exception.FeatureNotFoundException;
 
+/**
+ * Service for managing feature-related operations.
+ */
 @Service
 @AllArgsConstructor
 public class FeatureService {
@@ -22,6 +25,7 @@ public class FeatureService {
    *
    * @param featureDTO the DTO containing details for the new feature
    * @return the ID of the created feature
+   * @throws FailedToSaveEntityException if there was an error saving the feature to the repository
    */
   public long createFeature(CreateTagDTO featureDTO) {
     Feature feature = new Feature();
@@ -34,7 +38,7 @@ public class FeatureService {
   }
 
   /**
-   * Retrieves all features from the repository.
+   * Retrieves all features from the repository as FeatureDTOs.
    *
    * @return a list of all features
    */
@@ -64,6 +68,7 @@ public class FeatureService {
    * @param featureDTO the DTO containing updated details for the feature
    * @return a DTO representing the updated feature
    * @throws FeatureNotFoundException if no feature with the given ID exists
+   * @throws FailedToSaveEntityException if there was an error saving the updated feature to the repository
    */
   public FeatureDTO updateFeature(long id, CreateTagDTO featureDTO) {
     Feature feature = featureRepository.findById(id)
@@ -88,6 +93,12 @@ public class FeatureService {
     featureRepository.delete(feature);
   }
 
+  /**
+    * Retrieves a list of features that match the provided name.
+    * 
+    * @param feature the name to search for
+    * @return A list of features that match the provided name. If no features match, returns an empty list.
+    */
   public List<Feature> getFeatureByName(String feature) {
     return featureRepository.findAllByNameIgnoreCase(feature);
   }
