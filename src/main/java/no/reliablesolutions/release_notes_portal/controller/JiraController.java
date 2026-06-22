df4f172a47cb2e4a7e1b4f0bf22560ae2df63e42
Ludvik Lund-Hole <ludvik.hole@solwr.com>
2026-06-22 12:46:43 +0200

    ci: 💚 exclude Jira controller from ci profile

--- src/main/java/no/reliablesolutions/release_notes_portal/controller/JiraController.java
+++ src/main/java/no/reliablesolutions/release_notes_portal/controller/JiraController.java
@@ -3,6 +3,7 @@ package no.reliablesolutions.release_notes_portal.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import no.reliablesolutions.release_notes_portal.service.JiraService;
 @RestController
 @RequestMapping("/api/jira")
 @AllArgsConstructor
+@Profile("!ci")
 public class JiraController {
   private JiraService jiraService;
 
